f0915d0c7be6c5a4585b32be76b0bc93a35d6cd4
kirillgudkov <for.example.nickname@gmail.com>
2020-01-31 14:45:02 +0300

    fucked up with git add .

---
+++ README.md
@@ -0,0 +1,38 @@
+# eslint-config-react-native-talenttech
+Конфиг для ESLint в React Native проектах
+
+### Установить [ESLint](https://eslint.org):
+    yarn add eslint --dev
+    
+### Установить [Prettier](https://prettier.io):
+    yarn add prettier --dev --exact
+    # or globally
+    yarn global add prettier
+
+### Добавить в package.json, в раздел scripts:
+    "lint": "eslint ./src --ext .js,.jsx,.ts,.tsx",
+    "lint-fix": "eslint --fix ./src --ext .js,.jsx,.ts,.tsx",
+    "prettier": "npx prettier --write src/**/*.{js,jsx,ts,tsx,json}"
+
+### Создать файл .eslintrc и добавить в него:
+```
+{
+  "extends": "react-native-talenttech"
+}
+```
+
+### Для запуска линтера/исправлений:
+    yarn run lint
+    yarn run lint-fix
+
+### Pre-commit hooks:
+> - Установить `yarn add --dev husky`
+> - Выполнить `yarn add --dev pretty-quick`
+> - Добавить в package.json блок:
+> ```
+> "husky": {
+>    "hooks": {
+>      "pre-commit": "pretty-quick --staged && yarn lint-fix && yarn lint"
+>    }
+>  }
+> ```
---
+++ index.js
@@ -0,0 +1,41 @@
+module.exports = {
+	plugins: ['react', 'react-native', 'react-native-globals', '@typescript-eslint'],
+	extends: [
+		'./rules/style',
+		'prettier',
+		'prettier/@typescript-eslint',
+		'prettier/react'
+	].map(require.resolve),
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true,
+			modules: true,
+			experimentalObjectRestSpread: true,
+		}
+	},
+
+	rules: {
+		'dot-notation': 'error',
+		'semi': 'error',
+		'curly': ['error'],
+		'block-spacing': ['error', 'always'],
+		'jsx-quotes': ['error', 'prefer-double'],
+		'keyword-spacing': ['error', {before: true}],
+		'lines-between-class-members': ['error', 'always'],
+		'no-multiple-empty-lines': ['error', {
+			max: 1,
+			maxEOF: 1
+		}],
+		'no-underscore-dangle': ['error', {enforceInMethodNames: true}],
+		'operator-assignment': ['error', 'always'],
+		'indent': ['error', 2],
+		'brace-style': 'error',
+		'no-unused-vars': 'off',
+		'newline-per-chained-call': ['error', {ignoreChainWithDepth: 1}],
+		'react-native/sort-styles': 'off',
+		'react-native/no-inline-styles': 'off',
+		'react-native/no-unused-styles': 'warn',
+	}
+};
